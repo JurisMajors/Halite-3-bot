78877dc53b7cdd9e3955b02eb11f54d86d55f1fd
Juris Majors <j.a.majors@student.tue.nl>
2018-11-18 23:50:25 +0100

    made bot tournament tester

---
+++ GeneticAlgorithm/tester.py
@@ -0,0 +1,185 @@
+
+ # FITNESS: 1.1615033145815177, SCORE: 43978, PARAMETERS: [46, 50, 192, 0.97, 0.72, 644, 10, 263, 0.67, 0.37, 0.5, 0.17477422399760756] 
+ # FITNESS: 0.9395757511154426, SCORE: 61701, PARAMETERS: [85, 50, 64, 0.98, 0.47, 292, 7, 211, 0.27, 0.22, 0.85, 1.05] 
+ # FITNESS: 0.8893122845218654, SCORE: 39208, PARAMETERS: [85, 50, 64, 0.98, 0.47, 292, 7, 211, 0.27, 0.22, 0.85, 1.05] 
+ # FITNESS: 1.2665864464256413, SCORE: 46314, PARAMETERS: [253, 40, 92, 0.96, 0.33, 243, 10, 245, 0.35, 0.96, 0.72, 1.1] 
+ # FITNESS: 1.0570251267865376, SCORE: 36683, PARAMETERS: [491, 50, 68, 0.98, 0.47, 316, 9, 205, 0.01, 0.22, 0.26, 0.72] 
+ # FITNESS: 1.041364325831181, SCORE: 50804, PARAMETERS: [534, 39, 35, 0.75, 0.85, 253, 9, 260, 0.01, 0.22, 0.26, 0.54] 
+ # FITNESS: 1.1499102333931777, SCORE: 38430, PARAMETERS: [720, 50, 192, 0.97, 0.72, 253, 9, 260, 0.09, 2, 0.26, 0.72] 
+ # FITNESS: 1.014110697877926, SCORE: 37084, PARAMETERS: [704, 50, 192, 0.97, 0.85, 253, 9, 260, 0.09, 0.04, 1.05, 0.75] 
+ # FITNESS: 1.1883113153360139, SCORE: 41377, PARAMETERS: [910, 39, 102, 0.6, 0.85, 253, 9, 260, 0.01, 2, 1, 0.29000000000000004] 
+ # FITNESS: 1.0209584871097224, SCORE: 37899, PARAMETERS: [1038, 49, 96, 1.0, 0.99, 258, 10, 239, 0.45, 0.04, 1.05, 0.9100000000000001] 
+ # FITNESS: 1.0628647214854112, SCORE: 40070, PARAMETERS: [1259, 49, 35, 0.75, 0.85, 253, 9, 260, 0.09, 0.78, 1.05, 0.75] 
+ # FITNESS: 1.0300301890789683, SCORE: 38896, PARAMETERS: [1316, 49, 35, 0.75, 0.85, 253, 9, 220, 0.09, 0.78, 1.05, 0.75] 
+ # FITNESS: 1.0, SCORE: 51556, PARAMETERS: [0.1, 30, 50, 0.96, 0.99, 328, 11, 220, 0, 2, 1.0, 1.0] 
+ # FITNESS: 1.0215644551719758, SCORE: 64332, PARAMETERS: [1598, 50, 35, 0.75, 0.85, 253, 9, 260, 0.01, 0.37, 0.63, 0.29000000000000004] 
+ # FITNESS: 0.9691810977399472, SCORE: 52832, PARAMETERS: [1683, 49, 39, 0.87, 0.85, 253, 9, 260, 0.01, 2, 1.05, 0.66] 
+ # FITNESS: 0.9966400850611377, SCORE: 46867, PARAMETERS: [1895, 55, 109, 0.85, 0.63, 316, 10, 205, 0.09, 0.78, 1.05, 0.9] 
+ # FITNESS: 1.0311610024681983, SCORE: 43449, PARAMETERS: [2017, 49, 109, 0.8500000000000001, 0.85, 253, 9, 260, 0.01, 0.78, 1.05, 0.75] 
+ # FITNESS: 1.0261316909055307, SCORE: 57645, PARAMETERS: [2080, 50, 35, 0.75, 0.63, 253, 9, 205, 0.09, 0.78, 1.05, 0.75] 
+ # FITNESS: 0.9686607765113194, SCORE: 43087, PARAMETERS: [2175, 49, 35, 0.93, 0.85, 276, 9, 205, 0.01, 0.37, 1.05, 0.88] 
+ # FITNESS: 1.0, SCORE: 42680, PARAMETERS: [0.1, 30, 50, 0.96, 0.99, 328, 11, 220, 0, 2, 1.0, 1.0] 
+ # FITNESS: 1.106586952513893, SCORE: 42016, PARAMETERS: [2297, 49, 109, 0.85, 0.85, 253, 9, 260, 0.01, 0.37, 1.05, 0.75] 
+ # FITNESS: 1.0128689548321819, SCORE: 64067, PARAMETERS: [2520, 49, 109, 0.85, 1.0, 253, 9, 214, 0.01, 0.98, 1.05, 0.92] 
+ # FITNESS: 1.307358985787154, SCORE: 52799, PARAMETERS: [2666, 55, 109, 0.75, 0.99, 273, 9, 205, 0.01, 0.37, 1.05, 0.66] 
+ # FITNESS: 1.0102921578677602, SCORE: 49277, PARAMETERS: [2834, 55, 113, 0.85, 0.85, 270, 9, 260, 0.06999999999999999, 0.37, 1.05, 0.75] 
+ # FITNESS: 1.0246344355157424, SCORE: 54446, PARAMETERS: [2973, 49, 129, 0.87, 0.85, 290, 9, 221, 0.01, 0.98, 1.05, 0.92] 
+ # FITNESS: 1.18374615075646, SCORE: 44207, PARAMETERS: [2930, 49, 129, 0.87, 0.85, 290, 9, 221, 0.01, 0.98, 1.05, 0.92] 
+ # FITNESS: 1.0815925365450318, SCORE: 69329, PARAMETERS: [3255, 49, 122, 0.94, 0.85, 290, 9, 245, 0.01, 0.37, 1.05, 0.75] 
+ # FITNESS: 1.0811945090039627, SCORE: 64663, PARAMETERS: [3336, 49, 122, 0.94 0.85, 290, 9, 245, 0.01, 0.37, 1.05, 0.91] 
+ # FITNESS: 1.1056376677201416, SCORE: 58423, PARAMETERS: [3511, 49, 129, 0.87, 0.85, 290, 9, 245, 0.01, 0.37, 1.05, 0.75] 
+ # FITNESS: 1.0701793160353033, SCORE: 72632, PARAMETERS: [3447, 52, 129, 0.97, 0.98, 300, 9, 205, 0.01, 0.9, 1.05, 0.92] 
+ # FITNESS: 1.2670727152584251, SCORE: 44103, PARAMETERS: [3630, 49, 129, 0.87, 0.85, 290, 9, 221, 0.01, 0.37, 1.05, 0.75] 
+ # FITNESS: 1.0662865137575783, SCORE: 50301, PARAMETERS: [3866, 49, 109, 0.85, 0.9199999999999999, 290, 9, 221, 0.01, 0.37, 1.05, 0.92] 
+ # FITNESS: 1.1314793244030286, SCORE: 46626, PARAMETERS: [3987, 49, 129, 0.87, 0.85, 290, 9, 221, 0.27, 0.96, 1.05, 0.66] 
+ # FITNESS: 1.0786811924252682, SCORE: 52576, PARAMETERS: [4090, 49, 138, 0.87, 0.88, 290, 9, 205, 0.01, 0.96, 1.05, 0.99] 
+ # FITNESS: 1.236000797107638, SCORE: 43417, PARAMETERS: [4171, 49, 129, 0.94, 0.85, 290, 9, 221, 0.01, 0.94, 1.05, 0.66] 
+ # FITNESS: 0.9953990420292789, SCORE: 73774, PARAMETERS: [4213, 49, 129, 0.8700000000000001, 0.85, 290, 9, 205, 0.01, 0.37, 1.05, 0.75] 
+import random
+from math import log2
+from pathlib import Path
+import json
+import os
+
+OUR_BOT = [0, 30, 50, 0.7, 0.95, 300, 10, 220, 0, 2, 1, 0.8]	# 1.9
+OTHER_BOTS = [[46, 50, 192, 0.97, 0.72, 644, 10, 263, 0.67, 0.37, 0.5, 0.75], # 1.16
+ 				[253, 40, 92, 0.96, 0.33, 243, 10, 245, 0.35, 0.96, 0.72, 0.75], # 1.26
+ 				[720, 50, 192, 0.97, 0.72, 253, 9, 260, 0.09, 2, 0.26, 0.72], # 1.14
+ 				[910, 39, 102, 0.6, 0.85, 253, 9, 260, 0.01, 2, 1, 0.75],	# 1.18
+ 				[2297, 49, 109, 0.85, 0.85, 253, 9, 260, 0.01, 0.37, 1.05, 0.75],	# 1.10
+ 				[2666, 55, 109, 0.75, 0.99, 273, 9, 205, 0.01, 0.37, 1.05, 0.66], # 1.30
+ 				[2930, 49, 129, 0.87, 0.85, 290, 9, 221, 0.01, 0.98, 1.05, 0.92], # 1.18
+ 				[3511, 49, 129, 0.87, 0.85, 290, 9, 245, 0.01, 0.37, 1.05, 0.75], # 1.10
+ 				[3630, 49, 129, 0.87, 0.85, 290, 9, 221, 0.01, 0.37, 1.05, 0.75], # 1.26
+ 				[3987, 49, 129, 0.87, 0.85, 290, 9, 221, 0.27, 0.96, 1.05, 0.66], # 1.13
+ 				[4171, 49, 129, 0.94, 0.85, 290, 9, 221, 0.01, 0.94, 1.05, 0.66], # 1.23
+ 				[4090, 49, 138, 0.87, 0.88, 290, 9, 205, 0.01, 0.96, 1.05, 0.92], # 1.07
+ 				[3255, 49, 122, 0.94, 0.85, 290, 9, 245, 0.01, 0.37, 1.05, 0.75], # 1.08
+ 				[491, 50, 68, 0.98, 0.47, 316, 9, 205, 0.01, 0.22, 0.26, 0.72], # 1.06
+ 				[1259, 49, 35, 0.75, 0.85, 253, 9, 260, 0.09, 0.78, 1.05, 0.75] # 1.07
+ 				] 
+map_sizes = [32, 40, 48, 56, 64] # possible map sizes
+BASE_DIR = Path(os.path.dirname(__file__)).parent # path to halite.exe
+random.seed(1)
+TOURNAMENT_AMOUNT = 10
+DEPTH  = int(log2(len(OTHER_BOTS) + 1)) # tree depth is log of nodes
+stats = {} # version - >  list of ranking stats
+
+def run_iteration(variables1, variables2, seed): # run iteration of bots with variables
+	map_size= random.choice(map_sizes)
+	#print("MAP CHOICE: {}x{}".format(map_size, map_size))
+	bot1_variables = " ".join(map(str, variables1))
+	bot2_variables = " ".join(map(str, variables2))
+	cmd = '{0}\\halite.exe --replay-directory {0}\\replays/ --no-logs --results-as-json -vvv -s {2} --width {1} --height {1}'.format(BASE_DIR, map_size, seed)
+	cmd += ' "python {0}\\MyBot1.py {1}" "python {0}\\MyBot2.py {2}" > data.json'.format(BASE_DIR, bot1_variables, bot2_variables)
+	# save output in data.json, > for rewrite , >> for appending outputs
+	os.system(cmd) # run
+
+def get_result():
+	# get results from game
+	with open("data.json", "r") as f:
+		data = json.load(f)		
+	return data['stats']['0']['score'], data['stats']['1']['score']
+
+
+def init_matchings(all_bots):
+	''' assigns initial matchings, aka leaves of binary tree '''
+	bots = all_bots[:]
+	nr_of_pairs = int(len(bots)/2)
+	matchings = []
+
+	for _ in range(nr_of_pairs):
+		choice1 = random.choice(bots)
+		bots.remove(choice1)
+		choice2 = random.choice(bots)
+		bots.remove(choice2)
+		matchings.append((choice1, choice2))
+	return matchings
+
+def interim_matchings(bots):
+	nr_of_pairs = int(len(bots)/2)
+	matchings = []
+	for i in range(nr_of_pairs):
+		matchings.append((bots[2*i], bots[2*i + 1]))
+	return matchings
+
+def do_tournament(matchings):
+	''' runs a tournament with produces matchings and returns the rankings of the tournament'''
+	t_matchings = {} # DEPTH - > matchings
+	t_matchings[DEPTH] = matchings
+	seed = random.randint(0, 5000)
+	for lvl in range(DEPTH):
+		current_matchings = t_matchings[DEPTH - lvl]
+		bots_alive = []
+		# run current matching games
+		for match in current_matchings:
+			print("PLAYING MATCH {} VS {}".format(match[0][0], match[1][0]))
+			run_iteration(match[0], match[1], seed)
+			first, second = get_result()
+			if first >= second:
+				bots_alive.append(match[0])
+				print("WINNER: {} w SCORE {}".format(match[0][0], first))
+			else:
+				print("WINNER: {} w SCORE {}".format(match[1][0], second))
+				bots_alive.append(match[1])
+		if lvl == DEPTH - 1:
+			break # bots_alive contains only the winner
+		t_matchings[DEPTH - lvl - 1] = interim_matchings(bots_alive) # add next depth matchings
+	winner = bots_alive[0] 
+	print("TORUNAMENT WINNER: {}".format(winner))
+	return produce_rankings(t_matchings, winner[0])
+
+def produce_rankings(tournament, winner):
+	# takes dictionary of intermediate results for each level of depth n winner of tournament
+	# returns dictionary of rankings for each version rank -> [versions]
+	rankings = {}
+	rankings[1] = [winner]
+	rank = 2
+	for lvl in range(DEPTH):
+		matchings = tournament[lvl + 1]
+		bots_in_rank = []
+		for match in matchings:
+			if match[0][0] in rankings[rank - 1]: # determine loser
+				loser = match[1][0]
+			else:
+				loser = match[0][0]
+			bots_in_rank.append(loser) # add to the bots in this rank
+		rankings[rank] = bots_in_rank
+		rank += 1 # next rank
+	return rankings
+
+def produce_stats(rankings):
+	''' given rankings of tournament, produces statistics and adds to the already existing statistics'''
+	for rank, versions in rankings.items():
+		for version in versions:
+			statistics = stats[version] if version in stats.keys() else [0]*(DEPTH+1)
+			amount_scored = statistics[rank - 1]
+			statistics[rank - 1] = amount_scored + 1
+			stats[version] = statistics
+	return stats
+
+
+def write_stats():
+	# write stats in txt file
+	output = ["  V  1  2  3  4\n"]
+	for version, statistic in stats.items():
+		percentage = [round(x/TOURNAMENT_AMOUNT, 2) for x in statistic] # percentage of hittin that spot
+		line = [str(version)]
+		line.append(str(percentage))
+		line.append("\n")
+		s = "  ".join(line)
+		output.append(s)
+	final_string = "".join(output)
+
+	with open("results.txt", "w") as f:
+		f.write(final_string)
+
+
+OTHER_BOTS.append(OUR_BOT)
+for t in range(TOURNAMENT_AMOUNT):
+	matchings = init_matchings(OTHER_BOTS)	
+	rankings = do_tournament(matchings)
+	produce_stats(rankings)
+	print("TOURNAMENT {} FINISHED".format(t+1))
+write_stats()
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!FINISHED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
