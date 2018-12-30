8bf68f4867746bb4620b1c973a9f531cab43e731
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2018-12-30 20:00:19 +0100

    EXTRAA FILES FOR PROFILES
    sorry caps

--- run_game.bat
+++ run_game.bat
@@ -1,2 +1,2 @@
-halite.exe --replay-directory replays/ --height 32 --width  32 --turn-limit 100 -s 1546114291 -vvv "python MyBot.py" "python MyBot.py"
+halite.exe --replay-directory replays/ -s 1546192471 -vvv "python v58/58.py" "python MyBot.py" 
 pause
