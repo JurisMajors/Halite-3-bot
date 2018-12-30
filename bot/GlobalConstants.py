edada9b0cdb3e927b04e878e0e8a3f9a59819fb7
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2018-12-30 20:20:35 +0100

    some commenting not yet done

--- bot/GlobalConstants.py
+++ bot/GlobalConstants.py
@@ -6,6 +6,9 @@ from hlt import constants
 import json
 from decimal import Decimal
 
+""" This file contains the global variables used throughout the game 
+    Function is called in the main bot file """
+
 def load_global_constants(json_file):
 
     global PERCENTAGE_SWITCH, SMALL_PERCENTAGE, BIG_PERCENTAGE
@@ -18,25 +21,33 @@ def load_global_constants(json_file):
     with open(adress) as f:
         data = json.load(f)
 
-    # when switch collectable percentage of max halite
+
+    CRASH_TURN = constants.MAX_TURNS
+
+    ''' when switch collectable percentage of max halite '''
     PERCENTAGE_SWITCH = int(data["PERCENTAGE_SWITCH"] * constants.MAX_TURNS)
+
     SMALL_PERCENTAGE = float(data["SMALL_PERCENTAGE"])
     BIG_PERCENTAGE = float(data["BIG_PERCENTAGE"])
-    # definition of medium patch size for stopping and collecting patch if on
-    # the way
+
+    ''' definition of medium patch size for stopping and collecting patch if on the way '''
     MEDIUM_HALITE = int(data["MEDIUM_HALITE"])
-    # halite left at patch to stop collecting at that patch
+
+    ''' halite left at patch to stop collecting at that patch '''
     INITIAL_HALITE_STOP = int(data["INITIAL_HALITE_STOP"])
-    # until which turn to spawn ships
+    
+    ''' until which turn to spawn ships '''
     SPAWN_TURN = int(float(data["SPAWN_TURN"]) * constants.MAX_TURNS)
-    # Coefficients for halite heuristics
+    
+    ''' Coefficients for halite heuristics '''
     A = float(data["A"])
     B = float(data["B"])
     C = float(data["C"])
     D = float(data["D"])
     E = float(data["E"])
     F = float(data["F"])
-    CRASH_TURN = constants.MAX_TURNS
+
+
     CRASH_SELECTION_TURN = int(float(data["CRASH_SELECTION_TURN"]) * constants.MAX_TURNS)
 
     # Minimum halite needed to join a halite cluster
@@ -53,4 +64,5 @@ def load_global_constants(json_file):
     BUSY_RETURN_AMOUNT = data["BUSY_RETURN_AMOUNT"] * constants.MAX_TURNS
     UNSAFE_AREA = data["UNSAFE_AREA"]
 
-    MAX_SHIP_DROPOFF_RATIO = data["MAX_SHIP_DROPOFF_RATIO"]  # for each 40 ships there should be a dropoff
+    ''' for each 40 ships there should be a dropoff '''
+    MAX_SHIP_DROPOFF_RATIO = data["MAX_SHIP_DROPOFF_RATIO"]  
